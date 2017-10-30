b428f8a4e824c5040a8d98f0448fd0ce6292fe17
Tony Colston <tonetheman@gmail.com>
2017-10-30 18:55:07 -0500

    not working reading files in node is hard

--- crummy_index_server.js
+++ crummy_index_server.js
@@ -8,13 +8,47 @@ var io = require("socket.io")(http)
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
-
+app.get("/fakemain.js",function(req,res){
+	res.sendFile(__dirname + "/fakemain.js");
+})
 
 io.on("connection", function(socket) {
   console.log("got a connection on io");
   socket.on("start", function(data) {
       console.log("got a start",data);
-  });
+
+
+	var fs = require("fs");
+	var util = require("util");
+	var stream = require("stream");
+	var es = require("event-stream");
+
+	var line_number = 0;
+	var s = fs.createReadStream("../messages.txt")
+		.pipe(es.split())
+		.pipe(es.mapSync(function(line) {
+			var oper = line.substr(0,1);
+			s.pause();
+			console.log(oper);
+			if (oper == "T") {
+				// TODO:
+				// read the time from the start of the recording
+			}
+			if (oper == "M") {
+				// this is a message we need to send on
+				var index = line.indexOf("{");
+				var string_mesage = line.substring(index);
+				socket.emit("msg",string_message);
+			}
+			s.resume();
+		})
+		.on("error", function() {
+		})
+		.on("end", function() {
+			console.log("got an end to the file!");
+		})
+	);  
+	});
 
 })
 
